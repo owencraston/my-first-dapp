030f2905d6c5d7f2b7ffe1f155f5128c828d4316
owencraston <owen.craston@consensys.net>
2022-03-03 18:49:52 -0500

    install packages
    - downgraded react scripts to get passed the webpack error
    - setup basic function to get the provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
 
 function App() {
-  const [account, setAccount] = useState<undefined>(undefined)
+  const [account, setAccount] = useState<string|undefined>(undefined);
+
+  const loadBlockchainData = async () =>  {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    setAccount(accounts[0]);
+  }
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+  
   return (
     <div className="container">
       <h1>Hello, World!</h1>
