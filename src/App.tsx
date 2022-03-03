8165b3b7fbec315faceb03e8e026cd1ea328edf3
owencraston <owen.craston@consensys.net>
2022-03-03 18:20:34 -0500

    show account

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [account, setAccount] = useState<undefined>(undefined)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h1>Hello, World!</h1>
+      <p>Your account: {account}</p>
+  </div>
   );
 }
 
