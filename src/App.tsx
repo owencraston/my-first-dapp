98dfecbc03157df14fce37815f97f74b09634134
owencraston <owen.craston@consensys.net>
2022-03-03 19:43:14 -0500

    account is showing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Web3 from 'web3';
+import './App.css';
 
 function App() {
   const [account, setAccount] = useState<string|undefined>(undefined);
 
-  const loadBlockchainData = async () =>  {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-    const accounts = await web3.eth.getAccounts()
+  const web3Provider = 'http://localhost:7545';
+
+  const loadBlockchainData = async () => {
+    const web3 = new Web3(web3Provider);
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
     setAccount(accounts[0]);
-  }
+  };
 
   useEffect(() => {
     loadBlockchainData();
   }, []);
-  
+
   return (
     <div className="container">
       <h1>Hello, World!</h1>
-      <p>Your account: {account}</p>
-  </div>
+      <p>{`Your account: ${account}`}</p>
+    </div>
   );
 }
 
