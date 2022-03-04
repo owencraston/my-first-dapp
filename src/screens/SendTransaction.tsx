8177b2416a7f9e78a9b45ffc862928a05c86930a
owencraston <owen.craston@consensys.net>
2022-03-04 14:03:40 -0500

    basic routes

---
+++ src/screens/SendTransaction.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SendTransaction() {
+
+  return (
+    <div className="container">
+      <h1>SendTransaction</h1>
+    </div>
+  );
+}
+
+export default SendTransaction;
