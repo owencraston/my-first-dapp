3cd8517f29ee8dcdaa7021bea5089e751d124bca
owencraston <owen.craston@consensys.net>
2022-03-04 11:20:06 -0500

    cleaner type def

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,19 @@ import React, { useEffect, useState, useMemo} from 'react';
 import './App.css';
 import { ethers } from "ethers";
 
-declare var window: any
-
 function App() {
   // A Web3Provider wraps a standard Web3 provider, which is
   // what MetaMask injects as window.ethereum into each page
   const provider =  useMemo(() => new ethers.providers.Web3Provider(window.ethereum, "any"), [])
   const [account, setAccount] = useState<string|undefined>(undefined);
+  
   useEffect(() => {
-
   const loadAccounts = async () => {
     console.log("fetching adddress")
     // MetaMask requires requesting permission to connect users accounts
     await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner()
     const userAddress = await signer.getAddress();
-    console.log(userAddress)
     setAccount(userAddress)
   };
 
